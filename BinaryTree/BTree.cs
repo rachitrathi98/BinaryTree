dadad0873e479d382bd9783cdbfa3bd24bc1d342
Rachit Rathi <rachitrathi98@gmail.com>
2020-10-24 21:20:52 +0530

    [Rachit] Add. Create Binary Tree with three values

--- BinaryTree/BTree.cs
+++ BinaryTree/BTree.cs
@@ -4,7 +4,55 @@ using System.Text;
 
 namespace BinaryTree
 {
-    class BTree
+    class BTree<T> where T : IComparable<T>
     {
+        public T NodeData { get; set; }
+        public BTree<T> LeftTree { get; set; }
+        public BTree<T> RightTree { get; set; }
+        public BTree(T nodeData)
+        {
+            this.NodeData = nodeData;
+            this.LeftTree = null;
+            this.RightTree = null;
+        }
+        int leftCount = 0, rightCount = 0;
+        bool result = false;
+        public void Insert(T item)
+        {
+            T currentNodeValue = this.NodeData;
+            if ((currentNodeValue.CompareTo(item)) > 0)
+            {
+                if (this.LeftTree == null)
+                    this.LeftTree = new BTree<T>(item);
+                else
+                    this.LeftTree.Insert(item);
+            }
+            else
+            {
+                if (this.RightTree == null)
+                    this.RightTree = new BTree<T>(item);
+                else
+                    this.RightTree.Insert(item);
+            }
+        }
+        public void Display()
+        {
+            if (this.LeftTree != null)
+            {
+                this.leftCount++;
+                this.LeftTree.Display();
+            }
+            Console.WriteLine(this.NodeData.ToString());
+            if (this.RightTree != null)
+            {
+                this.rightCount++;
+                this.RightTree.Display();
+            }
+        }
+        public void GetSize()
+        {
+            Console.WriteLine("Size" + " " + (1 + this.leftCount + this.rightCount));
+        }
     }
 }
+
--- BinaryTree/Program.cs
+++ BinaryTree/Program.cs
@@ -6,6 +6,12 @@ namespace BinaryTree
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to BST code!");
+            BTree<int> binarySearch = new BTree<int>(56);
+            binarySearch.Insert(30);
+            binarySearch.Insert(70);
+            binarySearch.Display();
+            binarySearch.GetSize();
         }
     }
 }
